5c769c72808887af10667ba4156dfad1e0fb39cb
yousif30303 <yousif30303@gmail.com>
2026-04-13 09:29:56 +0400

    Integrate admin theme structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        then: function (): void {
+            Route::middleware('web')
+                ->group(base_path('routes/admin.php'));
+        },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
